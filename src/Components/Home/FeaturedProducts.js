8b6a19acce7d59ae652e26fb7f2130ff23f6aaf4
akazad1235 <akazad914@gmail.com>
2020-10-20 00:30:18 +0600

    Project Structure added

---
+++ src/Components/Home/FeaturedProducts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeaturedProducts = () => {
+    return (
+        <div>
+            <h2>featurd products</h2>
+        </div>
+    );
+};
+
+export default FeaturedProducts;
---
+++ src/Components/Home/NewArrival.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewArrival = () => {
+    return (
+        <div>
+            <h2>new Arrivial</h2>
+        </div>
+    );
+};
+
+export default NewArrival;
