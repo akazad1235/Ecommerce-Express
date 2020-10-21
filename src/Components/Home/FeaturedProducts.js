244c506da90999e0e409b08ed49dbd1516c3fe2b
akazad1235 <akazad914@gmail.com>
2020-10-21 12:26:10 +0600

    Category Product Added

--- src/Components/Home/FeaturedProducts.js
+++ src/Components/Home/FeaturedProducts.js
@@ -1,45 +1,9 @@
 import React from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import FakeData from '../../../src/fakeData/fakeData';
 
 const FeaturedProducts = () => {
-    const featuredProducts =[
-        {
-            id:1,
-            title:'Lenavo laptop model 54871',
-            price:'1500',
-            img:'https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg'
-        },
-        {
-            id:2,
-            title:'HP laptop model 548d71',
-            price:'5500',
-            img:'https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg'
-        },
-        {
-            id:3,
-            title:'Lenavo laptop model 54871',
-            price:'1500',
-            img:'https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg'
-        },
-        {
-            id:4,
-            title:'Lenavo laptop model 54871',
-            price:'1500',
-            img:'https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg'
-        },
-        {
-            id:5,
-            title:'Lenavo laptop model 54871',
-            price:'1500',
-            img:'https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg'
-        },
-        {
-            id:6,
-            title:'Lenavo laptop model 54871',
-            price:'1500',
-            img:'https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg'
-        }
-    ]
+    
     return (
        <>
         <Container fluid={true}>
@@ -47,9 +11,10 @@ const FeaturedProducts = () => {
             <p className="text-center section-sub-title">Some Of Our Exlusive Collection, You May Like</p>
             <Row>
                 {
-                    featuredProducts.map(fProducts => {
+                    FakeData.map(fProducts => {
                        return (
-                            <Col xl={2} lg={2} md={2} sm={4} sx={6}>
+                            <Col className="p-2" xl={2} lg={2} md={2} sm={4} xs={6}>
+                               
                                 <Card className="image-box card">
                                     <img src={fProducts.img} alt="" />
                                 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import Button from 'react-bootstrap/Button';
 import React from 'react';
 import FeaturedProducts from '../Components/Home/FeaturedProducts';
+import Categories from '../Components/Home/Categories';
 
 
 const Home = () => {
     return (
         <div>
             <FeaturedProducts></FeaturedProducts>
+            <Categories></Categories>/
         </div>
     );
 };
