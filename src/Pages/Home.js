11e1b8ee5dc67b293b0e3c67ad2d2aa70d8b663f
akazad1235 <akazad914@gmail.com>
2020-10-22 06:04:22 +0600

    Carousel Added in NewAriival Product

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,17 @@ import Button from 'react-bootstrap/Button';
 import React from 'react';
 import FeaturedProducts from '../Components/Home/FeaturedProducts';
 import Categories from '../Components/Home/Categories';
+import Collection from '../Components/Home/Collection';
+import NewArrival from '../Components/Home/NewArrival';
 
 
 const Home = () => {
     return (
         <div>
-            <FeaturedProducts></FeaturedProducts>
-            <Categories></Categories>/
+            <FeaturedProducts/>
+            <NewArrival/>
+            <Collection/>
+            <Categories/>
         </div>
     );
 };
--- src/fakeData/fakeData.js
+++ src/fakeData/fakeData.js
@@ -34,7 +34,20 @@
         title:'Lenavo laptop model 54871',
         price:'1500',
         img:'https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg'
-    }
+    },
+    {
+        id:'7',
+        title:'Lenavo laptop model 54871',
+        price:'1500',
+        img:'https://static-01.daraz.com.bd/p/e83135c4f08dba766d539651fb79d2fa.jpg_400x400q75.jpg'
+    },
+    {
+        id:'8',
+        title:'Lenavo laptop model 54871',
+        price:'1500',
+        img:'https://static-01.daraz.com.bd/p/66ec675d545eeabf8eb04415318d3db5.jpg_400x400q75-product.jpg'
+    },
+    
 ]
 
 export default FakeData;
